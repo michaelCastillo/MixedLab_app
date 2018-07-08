14a3d6da6b73d9df80c78e058904f94e7a8929f1
María Jesús <mjcamposandrade@gmail.com>
2018-07-08 01:45:14 -0400

    datos estaticos

--- Front/healthyFoodScrap/src/pages/profile/profile.ts
+++ Front/healthyFoodScrap/src/pages/profile/profile.ts
@@ -28,10 +28,10 @@ export class ProfilePage {
   public alertCtrl: AlertController) {
 
     this.formgroup = formbuilder.group({
-      name:['',Validators.required],
-      age:['',Validators.required],
-      heigth:[''],
-      weigth:[''],
+      name:['Rosa',Validators.required],
+      age:['50',Validators.required],
+      heigth:['160'],
+      weigth:['70'],
     });
 
     this.name = this.formgroup.controls['name'];
@@ -51,81 +51,10 @@ export class ProfilePage {
     console.log(this.formgroup.value['age']);
     console.log(this.formgroup.value['heigth']);
     console.log(this.formgroup.value['weigth']);
-    console.log(this.conditions);
+    //console.log(this.conditions);
+    alert("Datos Actualizados");
   }
 
-  /**
-   * Load itemsArray with object that will make up the select list
-   */
-  chooseItems() {
-    this.itemsArray = this.loadItemsArray();
-    if (this.selectedItems.length !== 0) {
-      /**
-       * selectedItems is not empty; iterate thru items array sending
-       * each item object to the setCheckedItems function to determine witch
-       * items need to be checked 
-       */
-      for (let i = 0; i < this.itemsArray.length; i++) {
-        this.setCheckedItems(this.itemsArray[i]);
-      }
-      this.createMultiSelectList();
-    } else {
-      // selectedItems is empty; create list with all items unchecked
-      this.createMultiSelectList();
-    }
-  }
-
-  /**
-   * Create an show the select list with the objects from the items Array
-   */
-  createMultiSelectList() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('Items');
-    for (let i = 0; i < this.itemsArray.length; i++) {
-      alert.addInput(this.itemsArray[i]);
-    }
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'Ok',
-      handler: data => {
-        this.selectedItems = data;
-        console.log(this.selectedItems);
-      }
-    });
-    alert.present();
-  }
-
-  /**
-   * Compares each item in the selctedItems array to each item
-   * int the itemsArray; If they match set checked to true
-   * 
-   * @param item 
-   */
-  setCheckedItems(item) {
-    for (let i = 0; i < this.selectedItems.length; i++) {
-      if (this.selectedItems[i] === item.value) {
-        item.checked = true;
-      }
-    }
-  }
-
-  /**
-   * Create initial items array will all item object having checked set to false
-   */
-  loadItemsArray(): Array<string> {
-    let items = [];
-    for (let i = 0; i < 5; i++) {
-      let item = i + 1;
-      let x = item.toString();
-      let itemObject = {
-        type: 'checkbox',
-        label: 'Item ' + x,
-        value: 'Item ' + x,
-        checked: false
-      };
-      items.push(itemObject);
-    }
-    return items;
-  }
+  
 
 }
