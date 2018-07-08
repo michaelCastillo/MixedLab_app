694f460d98c0789296685257159e5c5759fb0fdd
GarsePersen <sebastian.garay.p@usach.cl>
2018-07-08 03:08:30 -0400

    Se comienza a implementar perfil de producto

--- Front/healthyFoodScrap/src/components/product-list/product-list.ts
+++ Front/healthyFoodScrap/src/components/product-list/product-list.ts
@@ -12,24 +12,40 @@ export class ProductListComponent {
   icons: string[];
   items: Array<{name: string, pricesPerStore: Array<{price: Number, store: string}>}>;
   products: Array<string>;
-
+  slides: Array<Array<Array<{name: string, pricesPerStore: Array<{price: Number, store: string}>}>>>; 
+  slidesAux: Array<Array<{name: string, pricesPerStore: Array<{price: Number, store: string}>}>>;  
   constructor(public navCtrl: NavController) {
     // If we navigated to this page, we will have an item available as a nav param
     //this.selectedItem = navParams.get('item');
+    this.slidesAux = [];
+    this.slides = [];
 
     this.products = [];
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane','american-football', 'boat', 'bluetooth', 'build'];
     var products = ["tu","elfa","no","te","ama","te","pan","palta","tomate","lechuga"];
     var pricesPerStores_ = [{price:1000,store:"Jumbo"},{price:500,store:"Santa Isabel"},{price:1000,store:"Lider"}];
-
     this.items = [];
     for (let i = 0; i < 11; i++) {
+      if(i%6 == 0 && i != 0){
+        this.slides.push(this.slidesAux);
+        this.slidesAux= [];
+      }
+      this.items= [];
       this.items.push({
         name: products[i],
         pricesPerStore: pricesPerStores_,   
       });
+      this.slidesAux.push(this.items);
+      
     }
+    
+    if(this.slidesAux.length != 0){
+      this.slides.push(this.slidesAux); 
+      this.slidesAux=[];     
+    } 
+    console.log(this.slides);
+    
   }
 
   itemTapped(event, item) {
