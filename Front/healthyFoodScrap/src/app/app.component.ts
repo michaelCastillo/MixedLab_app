41afd204d306c031d13e58840cbd45ff5adba880
María Jesús <mjcamposandrade@gmail.com>
2018-07-08 08:19:37 -0400

    detalles font size

--- Front/healthyFoodScrap/src/app/app.component.ts
+++ Front/healthyFoodScrap/src/app/app.component.ts
@@ -24,10 +24,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'product', component: ProductPage },
-      { title: 'Profile', component: ProfilePage },
+      { title: 'Inicio', component: HomePage },
+      { title: 'Perfil', component: ProfilePage },
     ];
 
   }
